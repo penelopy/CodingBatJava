fd24561395e3b447cccb6daead680b91acce294c
Penelope Hill <penelope@squareup.com>
2015-09-30 17:53:36 -0700

    Adding exercises

---
+++ src/com/company/String1.java
@@ -0,0 +1,100 @@
+package com.company;
+
+
+public class String1 {
+
+
+    //Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+    public static String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+
+
+    /**
+     * Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string
+     * length will be at least 3.
+     */
+    public static String middleThree(String str) {
+        int center = str.length() / 2;
+
+        return new String(new char[]{
+                str.charAt(center - 1),
+                str.charAt(center),
+                str.charAt(center + 1),
+        });
+    }
+
+    /**Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars, and
+     otherwise return the string unchanged. This is a little harder than it looks.  Ex. xHi
+     */
+
+    public static String withoutX2(String str) {
+        char[] arr = str.toCharArray();
+        StringBuilder result = new StringBuilder();
+        if (str == "") {
+            return str;
+        }
+        else if (arr.length == 1) {
+            if (arr[0] != 'x') {
+                return str;
+            }
+            else {
+                return "";
+            }
+        }
+        else {
+            if (arr[0] != 'x') {
+                result.append(arr[0]);
+            }
+            if (arr[1] != 'x') {
+                result.append(arr[1]);
+            }
+            for (int i = 2; i < str.length(); i++) {
+                result.append(arr[i]);
+            }
+            return result.toString();
+        }
+
+    }
+
+    /**Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may
+    be any length. If there are fewer than 2 chars, use whatever is there.*/
+
+    /**
+     initialize new string
+     copy first char
+     copy 2nd char
+     repeat pattern 3 times
+     ignore all remaining letters
+     */
+
+    public static String extraFront(String str) {
+        char [] arr = str.toCharArray();
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+
+        if (str.length() >= 2) {
+            while (i <= 2) {
+                result.append(arr[0]);
+                result.append(arr[1]);
+                i++;
+            }
+            return result.toString();
+        }
+        else {
+            if (str.length() == 1) {
+                while (i <= 2) {
+                    result.append(arr[0]);
+                    i++;
+                }
+            return result.toString();
+            }
+            else {
+                return "";
+            }
+        }
+    }
+
+}
+
+
---
+++ test/com/company/String1Test.java
@@ -0,0 +1,45 @@
+package com.company;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class String1Test {
+
+
+    @Test
+    public void helloNameReturnsCorrectGreeting() {
+        assertThat(String1.helloName("Bob"), equalTo("Hello Bob!"));
+        assertThat(String1.helloName("Alice"), equalTo("Hello Alice!"));
+        assertThat(String1.helloName("X"), equalTo("Hello X!"));
+    }
+
+    @Test
+    public void middleThreeReturnsCorrectValue() {
+        assertThat(String1.middleThree("Candy"), equalTo("and"));
+        assertThat(String1.middleThree("and"), equalTo("and"));
+        assertThat(String1.middleThree("solving"), equalTo("lvi"));
+    }
+
+
+    @Test
+    public void withoutX2ReturnsCorrectValue() {
+        assertThat(String1.withoutX2("xHi"), equalTo("Hi"));
+        assertThat(String1.withoutX2("Hi"), equalTo("Hi"));
+        assertThat(String1.withoutX2("Hxo"), equalTo("Ho"));
+        assertThat(String1.withoutX2("x"), equalTo(""));
+        assertThat(String1.withoutX2(""), equalTo(""));
+    }
+
+    @Test
+    public void extraFront() {
+        assertThat(String1.extraFront("Hello"), equalTo("HeHeHe"));
+        assertThat(String1.extraFront("ab"), equalTo("ababab"));
+        assertThat(String1.extraFront("H"), equalTo("HHH"));
+        assertThat(String1.extraFront(""), equalTo(""));
+
+    }
+
+}
